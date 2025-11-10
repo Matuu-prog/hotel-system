dae10b4dbac291e4b3ba75173d802a36643da43f
Matuu-prog <carrizomateo1503@gmail.com>
2025-11-10 01:57:49 -0300

    Estilo negro

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import { initUsuarios } from "./utils/initData.js";
 initUsuarios();
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-datepicker/dist/react-datepicker.css';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 // App principal
 import App from "./App.jsx";
